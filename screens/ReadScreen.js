f9bdf72c41e0c0ca76a45127ee5383dd929a8144
xxvvccrr <88693779+xxvvccrr@users.noreply.github.com>
2021-08-11 17:00:10 -0700

    Add files via upload

---
+++ screens/ReadScreen.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { StyleSheet, Text, View, KeyboardAvoidingView, FlatList, ToastAndroid, ScrollView } from 'react-native';
+import {Header, SearchBar} from 'react-native-elements';
+import db from '../config'
+
+export default class ReadStoryScreen extends React.Component {
+    constructor(){
+      super();
+      this.state ={
+        allStories:[],
+        dataSource:[],
+        search : ''
+      }
+    }
+    componentDidMount(){
+      this.retrieveStories()
+    }
+  
+    updateSearch = search => {
+      this.setState({ search });
+    };
+  
+  
+    retrieveStories=()=>{
+      try {
+        var allStories= []
+        var stories = db.collection("stories")
+          .get().then((querySnapshot)=> {
+            querySnapshot.forEach((doc)=> {
+                // doc.data() is never undefined for query doc snapshots
+                
+                allStories.push(doc.data())
+                console.log('this are the stories',allStories)
+            })
+            this.setState({allStories})
+          })
+      }
+      catch (error) {
+        console.log(error);
+      }
+    };
+  
+  
+    SearchFilterFunction(text) {
+      //passing the inserted text in textinput
+      const newData = this.state.allStories.filter((item)=> {
+        //applying filter for the inserted text in search bar
+        const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      this.setState({
+        //setting the filtered newData on datasource
+        //After setting the data it will automatically re-render the view
+        dataSource: newData,
+        search: text,
+      });
+    }
+  
+      render(){
+        return(
+          <View style ={styles.container}>
+                <Header 
+                    backgroundColor = {'white'}
+                    leftComponent={{ icon: 'menu', color: 'purple' }}
+                    centerComponent = {{
+                        text : 'iSleep Stories',
+                        style : { color: 'purple', fontSize: 20}
+                    }}
+                    rightComponent={{ icon: 'home', color: 'purple' }}
+                />
+                <View styles ={{height:20,width:'100%'}}>
+              <SearchBar
+              placeholder="Type Here..."
+              onChangeText={text => this.SearchFilterFunction(text)}
+              onClear={text => this.SearchFilterFunction('')}
+              value={this.state.search}
+            />
+          </View>
+          
+          <FlatList
+                data={this.state.search === "" ?  this.state.allStories: this.state.dataSource}
+                renderItem={({ item }) => (
+                  <View style={styles.itemContainer}>
+                    <Text>  Title: {item.title}</Text>
+                    <Text>  Author : {item.author}</Text>
+                  </View>
+                )}
+                keyExtractor={(item, index) => index.toString()}
+                /> 
+          
+          
+          
+        </View>  
+      );      
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF'
+  },
+  item: {
+    backgroundColor: 'purple',
+    padding:10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  itemContainer: {
+    height: 80,
+    width:'100%',
+    borderWidth: 2,
+    borderColor: 'purple',
+    justifyContent:'center',
+    alignSelf: 'center',
+  }
+});
---
+++ screens/WriteScreen.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, KeyboardAvoidingView, ToastAndroid } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import {Header} from 'react-native-elements';
+import * as firebase from 'firebase';
+import db from '../config';
+
+export default class WriteScreen extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: '',
+            author: '',
+            storyText: '',
+        }
+    }
+
+    submitStory = ()=>{
+        db.collection("stories").add({
+            title: this.state.title,
+            author: this.state.author,
+            storyText: this.state.storyText,
+        })
+        this.setState({
+            title: '',
+            author: '',
+            storyText: ''
+        })
+        ToastAndroid.show('Your story has been sumitted', ToastAndroid.SHORT)
+    }
+
+    render(){
+        return(
+            <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+                <Header 
+                    backgroundColor = {'purple'}
+                    leftComponent={{ icon: 'menu', color: '#fff' }}
+                    centerComponent = {{
+                        text : 'iSleep Stories',
+                        style : { color: 'white', fontSize: 20}
+                    }}
+                    rightComponent={{ icon: 'home', color: '#fff' }}
+                />
+                <TextInput 
+                    placeholder="Story Title"
+                    onChangeText= {(text)=>{
+                        this.setState({
+                            title: text
+                        })
+                    }}
+                    value={this.state.title}
+                    style={styles.title}/>
+                <TextInput
+                    placeholder="Author"
+                    onChangeText= {(text)=>{
+                        this.setState({
+                            author: text
+                        })
+                    }}
+                    value={this.state.author}
+                    style={styles.author} />
+                <TextInput 
+                    placeholder="Write your story"
+                    onChangeText= {(text)=>{
+                        this.setState({
+                            storyText: text
+                        })
+                    }}
+                    value={this.state.storyText}
+                    style={styles.storyText}
+                    multiline={true}/>
+                
+                <TouchableOpacity
+                    style={styles.submitButton}
+                    onPress={this.submitStory}>
+                    <Text style={styles.buttonText}>Submit</Text>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  title:{
+    height: 40,
+    borderWidth: 2,
+    marginTop: 40,
+    margin: 10,
+    paddingLeft: 5
+},
+author: {
+    height: 40,
+    borderWidth: 2,
+    margin: 10,
+    paddingLeft: 5
+},
+storyText: {
+    height: 250,
+    borderWidth: 2,
+    margin: 10,
+    paddingLeft: 5
+},
+submitButton:{
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'purple',
+    width: 80,
+    height: 40
+},
+buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 17
+}
+});
